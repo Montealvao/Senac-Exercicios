3b4a0bb16fc4d079f4c0860b7505e994c936b7e7
Guilherme Montealvão <guilhermehmc06@gmail.com>
2024-11-01 15:03:11 -0400

    alterado

--- Desenvolvimento/Atividade em grupo/scriptinsano.js
+++ Desenvolvimento/Atividade em grupo/scriptinsano.js
@@ -27,3 +27,23 @@ window.onload = function(){
 
     começo (duraçao,local)
 }
+
+const botao = document.getElementById("but");
+botao.style.backgroundColor = "red"
+
+var t = 0
+
+botao.addEventListener("click", function(){
+    if (t == 0){
+        botao.innerText = "Inscrito"
+        botao.style.backgroundColor = "green"
+        botao.style.filter = "drop-shadow( 0px 0px 5px green)"
+        t++
+    }
+    else{
+        botao.innerText = "Inscreva-se"
+        botao.style.backgroundColor = "red"
+        botao.style.filter = "drop-shadow( 0px 0px 5px red)"
+        t--
+    }
+});
